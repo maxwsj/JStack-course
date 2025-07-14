42b4e48834bca47b5c349834a642bffd142ec2d5
maxwsj <mw2study@gmail.com>
2025-07-13 21:37:13 -0300

    add useEffect lesson

--- React/react-hooks-basic-concepts/hooks-lessons/src/App.jsx
+++ React/react-hooks-basic-concepts/hooks-lessons/src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
    const [counter, setCounter] = useState(0);
+   const [name, SetName] = useState('');
+
+   useEffect(() => {
+      // IIFE -> Immediately Invoked Function Expression - Executar chamadas de APIs
+      (async () => {
+         // API call
+      })();
+   }, []);
+
+   useEffect(() => {
+      // Determina o que fazer quando o componente for sair da tela
+      return () => {};
+   });
 
    function handlePlus() {
       setCounter((prevState) => prevState + 1);
@@ -19,7 +32,12 @@ function App() {
             </button>
             <button onClick={handleMinus}>-</button>
          </div>
+         <br />
          <div>{counter}</div>
+         <br />
+         <span>{name}</span>
+         <br />
+         <input onChange={(e) => SetName(e.target.value)} />
       </>
    );
 }
