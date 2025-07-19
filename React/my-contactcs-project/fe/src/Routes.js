59901d17510c47392ac6ba165225c1d1d53f4364
maxwsj <mw2study@gmail.com>
2025-07-18 21:53:16 -0300

    configure the React Router

---
+++ React/my-contactcs-project/fe/src/Routes.js
@@ -0,0 +1,14 @@
+import EditContact from "pages/EditContact";
+import Home from "pages/Home";
+import NewContact from "pages/NewContact";
+import { Switch, Route } from "react-router-dom";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/new" component={NewContact} />
+      <Route path="/edit/:id" component={EditContact} />
+    </Switch>
+  );
+}
--- React/my-contactcs-project/fe/src/components/App/index.js
+++ React/my-contactcs-project/fe/src/components/App/index.js
@@ -3,17 +3,20 @@ import GlobalStyles from "../../assets/styles/global";
 import defaultTheme from "../../assets/styles/themes/default";
 import { Container } from "./styles";
 import Header from "components/Header";
-import ContactsList from "components/ContactsList";
+import Routes from "../../Routes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
