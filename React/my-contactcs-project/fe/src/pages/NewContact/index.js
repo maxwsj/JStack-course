3a0de2fed4250a742b005f112f6944f5a85841c8
maxwsj <mw2study@gmail.com>
2025-07-19 18:20:46 -0300

    create Button component

--- React/my-contactcs-project/fe/src/pages/NewContact/index.js
+++ React/my-contactcs-project/fe/src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import Input from "components/Input";
 import Select from "components/Select";
 import PageHeader from "components/PageHeader";
+import Button from "components/Button";
 
 export default function NewContact() {
   return (
@@ -12,6 +13,7 @@ export default function NewContact() {
         <option value="123">Instagram</option>
         <option value="123">Instagram</option>
       </Select>
+      <Button type="button">Salvar alterações</Button>
     </>
   );
 }
