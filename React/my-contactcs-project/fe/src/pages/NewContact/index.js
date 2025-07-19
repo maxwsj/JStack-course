de3a730a3c0e1d6b9ca7b56e91b3a08ad4a49a56
maxwsj <mw2study@gmail.com>
2025-07-19 16:01:33 -0300

    create Input and Select component

--- React/my-contactcs-project/fe/src/pages/NewContact/index.js
+++ React/my-contactcs-project/fe/src/pages/NewContact/index.js
@@ -1,5 +1,17 @@
+import Input from "components/Input";
+import Select from "components/Select";
 import PageHeader from "components/PageHeader";
 
 export default function NewContact() {
-  return <PageHeader title="Novo contato" />;
+  return (
+    <>
+      <PageHeader title="Novo contato" />
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
+  );
 }
