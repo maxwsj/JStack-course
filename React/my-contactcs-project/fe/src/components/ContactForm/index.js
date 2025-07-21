f72ddc496e5fbf6025e05e9806d2c8e4dda67543
maxwsj <mw2study@gmail.com>
2025-07-21 02:10:08 -0300

    add name and email input validation logic

--- React/my-contactcs-project/fe/src/components/ContactForm/index.js
+++ React/my-contactcs-project/fe/src/components/ContactForm/index.js
@@ -1,17 +1,66 @@
 import PropTypes from "prop-types";
-import Input from "components/Input";
+import { useState } from "react";
+
+import isEmailValid from "utils/isEmailValid";
+
 import { Form, ButtonContainer } from "./styles";
 
+import Input from "components/Input";
 import FormGroup from "components/FormGroup";
 import Select from "components/Select";
 import Button from "components/Button";
-import { useState } from "react";
-
 export default function ContactForm({ buttonLabel }) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [category, setCategory] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: "name", message: "Nome é obrigatório." },
+      ]);
+    } else {
+      setErrors((prevState) => [
+        prevState.filter((error) => error.field !== "name"),
+      ]);
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find(
+        (error) => error.field === "email"
+      );
+
+      if (errorAlreadyExists) return;
+
+      setErrors((prevState) => [
+        ...prevState,
+        { field: "email", message: "E-mail inválido." },
+      ]);
+    } else {
+      setErrors((prevState) => [
+        prevState.filter((error) => error.field !== "email"),
+      ]);
+    }
+  }
+
+  console.log(errors);
+
+  function handlePhoneChange(event) {
+    setPhone(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -20,33 +69,26 @@ export default function ContactForm({ buttonLabel }) {
   return (
     <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input
-          placeholder="Nome"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
+        <Input placeholder="Nome" value={name} onChange={handleNameChange} />
       </FormGroup>
 
       <FormGroup>
         <Input
           placeholder="E-mail"
           value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          onChange={handleEmailChange}
         />
       </FormGroup>
       <FormGroup>
         <Input
           placeholder="Telefone"
           value={phone}
-          onChange={(event) => setPhone(event.target.value)}
+          onChange={handlePhoneChange}
         />
       </FormGroup>
 
       <FormGroup>
-        <Select
-          value={category}
-          onChange={(event) => setCategory(event.target.value)}
-        >
+        <Select value={category} onChange={handleCategoryChange}>
           <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
           <option value="discord">Discord</option>
