ccf387e9903cac0f29da2f164bf1f37b4a12a3cd
maxwsj <mw2study@gmail.com>
2025-07-29 21:50:58 -0300

    show no contact found message

--- React/my-contactcs-project/fe/src/pages/Home/index.js
+++ React/my-contactcs-project/fe/src/pages/Home/index.js
@@ -8,6 +8,7 @@ import {
   Card,
   ErrorContainer,
   EmptyListContainer,
+  SearchNotFoundContainer,
 } from "./styles";
 
 import arrow from "../../assets/images/icons/arrow.svg";
@@ -15,6 +16,7 @@ import edit from "../../assets/images/icons/edit.svg";
 import trash from "../../assets/images/icons/trash.svg";
 import sad from "../../assets/images/sad.svg";
 import emptyBox from "../../assets/images/empty-box.svg";
+import magnifierQuestion from "../../assets/images/magnifier-question.svg";
 
 import Modal from "components/Modal";
 import Loader from "components/Loader";
@@ -44,8 +46,7 @@ export default function Home() {
     try {
       setIsLoading(true);
 
-      // const contactsList = await ContactsService.listContacts(orderBy);
-      const contactsList = [];
+      const contactsList = await ContactsService.listContacts(orderBy);
       await ContactsService.listContacts(orderBy);
 
       setHasError(false);
@@ -130,6 +131,17 @@ export default function Home() {
                 o seu primeiro!
               </p>
             </EmptyListContainer>
+          )}
+
+          {contacts.length > 0 && filteredContacts.length < 1 && (
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier question" />
+
+              <span>
+                Nenhum resultado foi encontrado para{" "}
+                <strong>{searchTerm}</strong>
+              </span>
+            </SearchNotFoundContainer>
           )}
 
           {filteredContacts.length > 0 && (
