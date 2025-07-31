5b854f98bea6fab7c644659287af93708dc15dcf
maxwsj <mw2study@gmail.com>
2025-07-30 23:36:18 -0300

    refactor the HttpClient code

--- React/my-contactcs-project/fe/src/services/utils/HttpClient.js
+++ React/my-contactcs-project/fe/src/services/utils/HttpClient.js
@@ -6,33 +6,45 @@ class HttpClient {
     this.baseURL = baseURL;
   }
 
-  async get(path) {
-    await delay(2000);
-
-    const response = await fetch(`${this.baseURL}${path}`);
-
-    let body = null;
-
-    const contentType = response.headers.get("Content-Type");
-
-    if (contentType.includes("application/json")) {
-      body = await response.json();
-    }
-
-    if (response.ok) {
-      return body;
-    }
-
-    throw new APIError(response, body);
+  get(path, options) {
+    return this.makeRequest(path, { method: "GET", headers: options?.headers });
   }
 
-  async post(path, body) {
+  post(path, options) {
+    return this.makeRequest(path, {
+      method: "POST",
+      body: options?.body,
+      headers: options?.headers,
+    });
+  }
+
+  put(path, body) {
+    return this.makeRequest(path, { method: "PUT", body });
+  }
+
+  async makeRequest(path, options) {
     await delay(2000);
 
-    const headers = new Headers({ "Content-Type": "application/json" });
+    const { method, body } = options;
+
+    const headers = new Headers();
+
+    if (options.body) {
+      headers.append("Content-Type", "application/json");
+    }
+
+    if (options.headers) {
+      // Object.keys(options.headers).forEach((name) => {
+      //   headers.append(name, options.headers[name]);
+      // });
+
+      Object.entries(options.headers).forEach(([name, value]) => {
+        headers.append(name, value);
+      });
+    }
 
     const response = await fetch(`${this.baseURL}${path}`, {
-      method: "POST",
+      method: method,
       body: JSON.stringify(body),
       headers,
     });
