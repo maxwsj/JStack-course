a4770fad699da7f4f3bc0debcd8d474dad73cb1d
maxwsj <mw2study@gmail.com>
2025-08-08 00:06:44 -0300

    refactor EventManager to use Map

--- React/my-contactcs-project/fe/src/lib/EventManager.js
+++ React/my-contactcs-project/fe/src/lib/EventManager.js
@@ -1,29 +1,30 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {};
+    this.listeners = new Map();
   }
 
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
-    this.listeners[event].push(listener);
+
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
   }
 
   removeListener(event, listenerToRemove) {
-    const listeners = this.listeners[event];
+    const listeners = this.listeners.get(event);
 
-    if (!listeners[event]) {
+    if (!listeners) {
       return;
     }
 
@@ -31,12 +32,6 @@ export default class EventManager {
       (listener) => listener !== listenerToRemove
     );
 
-    this.listeners[event] = filteredListeners;
+    this.listeners.set(event, filteredListeners);
   }
 }
-
-const toastEventManager = new EventManager();
-
-toastEventManager.on("addtoast", () => {});
-
-console.log(toastEventManager);
