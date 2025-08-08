c01751d701d38d261d5e210c80acddafe3e63707
maxwsj <mw2study@gmail.com>
2025-08-07 23:00:56 -0300

    create methods to add listeners and emit events

---
+++ React/my-contactcs-project/fe/src/lib/EventManager.js
@@ -0,0 +1,28 @@
+export default class EventManager {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, listener) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(listener);
+  }
+
+  emit(event, payload) {
+    if (!this.listeners[event]) {
+      return;
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(payload);
+    });
+  }
+}
+
+const toastEventManager = new EventManager();
+
+toastEventManager.on("addtoast", () => {});
+
+console.log(toastEventManager);
