109e2790e6ceef7fab05592c7fd8378f72bec90f
maxwsj <mw2study@gmail.com>
2025-08-10 20:55:13 -0300

    clear form input data after submit

--- React/my-contactcs-project/fe/src/components/ContactForm/index.js
+++ React/my-contactcs-project/fe/src/components/ContactForm/index.js
@@ -77,6 +77,10 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
     await onSubmit({ name, email, phone, categoryId });
 
     setIsSubmitting(false);
+    setName("");
+    setEmail("");
+    setPhone("");
+    setCategoryId("");
   }
 
   return (
