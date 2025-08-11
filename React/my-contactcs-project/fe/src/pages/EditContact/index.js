a9d5c37589500093f41bd440b60174e29d2c7151
maxwsj <mw2study@gmail.com>
2025-08-10 21:15:16 -0300

    load contact data at EditContact page

--- React/my-contactcs-project/fe/src/pages/EditContact/index.js
+++ React/my-contactcs-project/fe/src/pages/EditContact/index.js
@@ -1,11 +1,44 @@
-import ContactForm from "components/ContactForm";
+import { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+
 import PageHeader from "components/PageHeader";
+import Loader from "components/Loader";
+import ContactForm from "components/ContactForm";
+
+import ContactsService from "services/ContactsService";
+import toast from "utils/toast";
 
 export default function EditContact() {
+  const [isLoading, setisLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        console.log(contactData);
+        setisLoading(false);
+      } catch {
+        history.push("/");
+        toast({ type: "danger", text: "Contato não encontrado!" });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {}
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader title="Editar Max William" />
-      <ContactForm buttonLabel="Salvar alterações" />
+
+      <ContactForm buttonLabel="Salvar alterações" onSubmit={handleSubmit} />
     </>
   );
 }
