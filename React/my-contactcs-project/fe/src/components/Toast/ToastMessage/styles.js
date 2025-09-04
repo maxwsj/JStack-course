3f06e78ab8c557d429b6c075d82cd72196eca75e
maxwsj <mw2study@gmail.com>
2025-09-04 00:12:36 -0300

    animate list entrance

--- React/my-contactcs-project/fe/src/components/Toast/ToastMessage/styles.js
+++ React/my-contactcs-project/fe/src/components/Toast/ToastMessage/styles.js
@@ -1,4 +1,15 @@
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
+
+const messageIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+`;
 
 const containerVariants = {
   default: css`
@@ -22,6 +33,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${messageIn} 0.3s ease forwards;
 
   ${({ type }) => containerVariants[type] || containerVariants.default}
 
