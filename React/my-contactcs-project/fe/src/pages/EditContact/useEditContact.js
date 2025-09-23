2d6936676d47de28bb425c948989eebde39ea455
maxwsj <mw2study@gmail.com>
2025-09-23 00:21:07 -0300

    add useHistory substitutes

--- React/my-contactcs-project/fe/src/pages/EditContact/useEditContact.js
+++ React/my-contactcs-project/fe/src/pages/EditContact/useEditContact.js
@@ -1,6 +1,6 @@
 import useSafeAsyncAction from "hooks/useSafeAsyncAction";
 import { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ContactsService from "services/ContactsService";
 import toast from "utils/toast";
 
@@ -11,7 +11,7 @@ export default function useEditContact() {
   const contactFormRef = useRef(null);
 
   const { id } = useParams();
-  // const history = useHistory();
+  const navigate = useNavigate();
   const safeAsyncAction = useSafeAsyncAction();
 
   useEffect(() => {
@@ -34,7 +34,7 @@ export default function useEditContact() {
           return;
         }
         safeAsyncAction(() => {
-          // history.push("/");
+          navigate("/", { replace: true });
           toast({ type: "danger", text: "Contato não encontrado!" });
         });
       }
@@ -45,7 +45,7 @@ export default function useEditContact() {
     return () => {
       controller.abort();
     };
-  }, [id, safeAsyncAction]);
+  }, [id, navigate, safeAsyncAction]);
 
   async function handleSubmit(contact) {
     try {
